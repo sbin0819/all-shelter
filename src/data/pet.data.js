27ad827a65f32da142d183b11b332089910825c6
sbinha0819 <sbinha123@gmail.com>
2020-05-03 15:11:50 +0900

    detail

--- src/data/pet.data.js
+++ src/data/pet.data.js
@@ -6,6 +6,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 2,
@@ -14,6 +16,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1520087619250-584c0cbd35e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 4,
@@ -30,6 +36,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 5,
@@ -38,6 +46,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 6,
@@ -46,6 +56,7 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 7,
@@ -54,6 +65,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1534521304921-99d19068d8aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 8,
@@ -62,6 +75,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/photo-1491485880348-85d48a9e5312?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
   {
     id: 9,
@@ -70,6 +85,8 @@ const PET_DAT = [
     imageUrl:
       'https://images.unsplash.com/uploads/1412433710756bfa9ec14/d568362b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     price: 25,
+    available: true,
+    text: `Soondol was born in a house which had sold dogs to a dog meat restaurant.He was once adopted, but due to the owner’s personal circumstances, he was abandoned again.Soondol is very smart and gentle.`,
   },
 ];
 
--- src/pages/AnimalDetail.js
+++ src/pages/AnimalDetail.js
@@ -2,12 +2,66 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
+import PET_DATA from '../data/pet.data';
+
+const Container = styled.div`
+  position: relative;
+  top: 95px;
+  margin: 0px 20px;
+`;
+
+const BasicInfo = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const Img = styled.div`
+  height: 400px;
+  width: 400px;
+  margin-right: 30px;
+  background-position: center;
+  background-size: cover;
+`;
+
+const InfoBox = styled.div`
+  height: 400px;
+  width: 400px;
+  background-color: lightpink;
+  h3 {
+    margin-bottom: 20px;
+  }
+`;
+
+const Introduce = styled.div`
+  height: 100px;
+  background-color: pink;
+  width: 830px;
+`;
+
 const AnimalDetail = ({ match }) => {
+  const { animalId } = match.params;
+  const info = PET_DATA.find((e) => e.id === Number(animalId));
   return (
-    <div>
-      <div style={{ height: '200px', backgroundColor: 'pink' }}>하이</div>
-      <div>{match.params.animalId}</div>
-    </div>
+    <Container>
+      <div>
+        <BasicInfo>
+          <Img
+            style={{
+              backgroundImage: `url(${info.imageUrl})`,
+            }}
+          />
+          <InfoBox>
+            <h3>Info1</h3>
+            <h3>Info1</h3>
+            <h3>Info1</h3>
+            <h3>Info1</h3>
+            <h3>Info1</h3>
+            <h3>Info1</h3>
+          </InfoBox>
+        </BasicInfo>
+        <Introduce>{info.text}</Introduce>
+      </div>
+    </Container>
   );
 };
 
